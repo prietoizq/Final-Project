a2cf3d535613eb05efceab608f64848ee63a74a4
Angel <prietoizq@gmail.com>
2015-06-03 15:25:37 +0200

    Login y Logout funcionando

--- final_project/config/routes.rb
+++ final_project/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users do
     resources :images
   end
